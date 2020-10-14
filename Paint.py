7413ecc02acf7313bfe3bba76509531156d3655e
Irvin Lopez <irvin_21_95@hotmail.com>
2020-10-14 08:43:29 -0600

    Print and create board with O's

--- Paint.py
+++ Paint.py
@@ -1,13 +1,16 @@
 class paint:
     
-    board = []
+    __board = []
     continueOpt = True
 
     def command_request(self):
         option = input('> ').split()
 
         if option[0] == 'I' and option[1].isnumeric() and option[2].isnumeric():
-            print('ok')
+            self.new_image(option[1], option[2])
+
+        elif option[0] == 'S':
+            print(self.__board)
 
         elif option[0] == 'X':
             print('bye')
@@ -15,3 +18,10 @@ class paint:
 
         else:
             print('Command not found')
+
+    def new_image(self, Mx, Ny):
+        for i in range(int(Ny)):
+            row = []
+            for j in range(int(Mx)):
+                row.append('O')
+            self.__board.append(row)                
